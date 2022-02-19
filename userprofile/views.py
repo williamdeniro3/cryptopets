14231388e9d17a448db6383cd3ae55e185741512
William DeNiro <deniro.12@osu.edu>
2022-02-19 13:42:04 -0500

    added api calls and 1d change dictionary

--- userprofile/views.py
+++ userprofile/views.py
@@ -5,18 +5,28 @@ import requests
 
 def index(request):
     ##client = Client(<api_key>, <api_secret>, api_version='YYYY-MM-DD')
-    url = "https://api.prime.coinbase.com/v1/portfolios/APIKEYHERE/balances?"
+    url = "https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=EUR"
 
-    headers = {
-        "Accept": "application/json",
-        "X-CB-ACCESS-KEY": "$ACCESS_KEY",
-        "X-CB-ACCESS-PASSPHRASE": "$PASSPHRASE",
-        "X-CB-ACCESS-SIGNATURE": "$SIGNATURE",
-        "X-CB-ACCESS-TIMESTAMP": "$TIMESTAMP"
-    }
 
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers={})
+    jsonResponse = response.json()
 
-    print(response.text)
+    dict = {}
+  
+    for coin in range(len(jsonResponse['coins'])):
+       coin_name = jsonResponse['coins'][coin]['id']
+       coin_price = jsonResponse['coins'][coin]['price']
+       coin_1d_change = jsonResponse['coins'][coin]['priceChange1d']
+       dict[coin_name] = {
+            'coin_price' : coin_price,
+            'priceChange1d' : coin_1d_change
+        }
 
-    return HttpResponse("Hello, world. You're at the crypto pets index.")
+    array = []
+    for key in dict:
+       if(dict[key]['priceChange1d'] < 1):
+           array.append(key + "= :(")
+       else:
+           array.append(key + "= :)")
+
+    return HttpResponse('\n'.join(map(str, array)))
