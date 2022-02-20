1cb8d11ea7a770f7ab370c6a1359a0b5cd495903
William DeNiro <deniro.12@osu.edu>
2022-02-20 13:49:19 -0500

    added pet model

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,3 +4,8 @@ from django.forms import CharField
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=200)
+
+class Pet(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=200)
+
